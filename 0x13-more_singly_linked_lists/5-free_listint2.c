8c74f4cb97884394d1d3c9b84dbff371c4e23684
hot-shot7 <amansis2003@gmail.com>
2023-12-12 05:01:06 -0800

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+
+void free_listint2(listint_t **head)
+{
+listint_t *cursor;
+listint_t **temp = head;
+
+if (temp != NULL)
+{
+while (*head != NULL)
+{
+cursor = *head;
+free(cursor);
+*head = (*head)->next;
+}
+
+*temp = NULL;
+
+}
+}
